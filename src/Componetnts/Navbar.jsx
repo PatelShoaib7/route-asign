e120047cc94ffc23e1cd76bfabe0bed4a250b30f
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2022-12-22 07:50:25 +0530

    Update Navbar.jsx

--- src/Componetnts/Navbar.jsx
+++ src/Componetnts/Navbar.jsx
@@ -3,7 +3,6 @@ import {Link , useNavigate} from "react-router-dom"
 
 export const Navbar = () => {
  const navigate = useNavigate()
- 
  const open_1 =(id)=>
  {
        if(id === 1)
@@ -18,21 +17,14 @@ export const Navbar = () => {
      navigate("products/:id/:name")
     }
  }
- 
- 
   return (
     <div style={{border:'1px solid black', fontSize:'20px', width:'80%'}}>
-       
     <a  style={{marginLeft:'20px'}} href='/'>Home</a>
     <a  style={{marginLeft:'20px'}}href='/about'>About</a> 
     <a  style={{marginLeft:'20px'}} href='/products'>Products</a>
     <a  style={{marginLeft:'20px'}} href='/sign'>Sign_Up</a> 
-    {/* <a    style={{marginLeft:'20px'}} href='/Prodct'>Prodct</a> */}
- 
    <button onClick={()=>open_1(1)}>Go to Products </button>
-
    <button  onClick={()=>open_2(2)}>Go to Prodct </button>
-    
     </div>
   )
 }
